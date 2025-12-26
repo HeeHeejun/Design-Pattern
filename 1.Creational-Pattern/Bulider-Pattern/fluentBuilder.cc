ef0fb5af114a9d400a1b92833067ff41cd86658e
HeeHeejun <gmlwns123924@gmail.com>
2025-12-27 03:19:43 +0900

    code : builder의 기본

---
+++ 1.Creational-Pattern/Bulider-Pattern/fluentBuilder.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+
+struct HtmlElement
+{
+    std::string name;
+    std::string text;
+
+    std::vector<HtmlElement> elements;
+
+    HtmlElement() = default;
+    HtmlElement(const std::string& name, const std::string& text)
+        : name(name), text(text) {};
+
+    std::string str(int indent = 0) const
+    {
+        std::string result(indent, ' ');
+        result += "<" + name + ">" + text;
+        for (const auto& child : elements)
+        {
+            result += "\n" + child.str(indent + 2);
+        }
+        result += "\n" + std::string(indent, ' ') + "</" + name + ">";
+        return result;
+    }
+};
+
+struct HtmlBuilder
+{
+    HtmlElement root;
+    HtmlBuilder(const std::string& root_name)
+    {
+        root.name = root_name;
+    }
+
+    HtmlBuilder& add_child(std::string child_name, std::string child_text)
+    {
+        HtmlElement e{child_name,child_text};
+
+        root.elements.emplace_back(e);
+
+        return *this;
+    };
+    std::string str(){return root.str();}
+};
+
+int main()
+{
+    HtmlBuilder bd{"ul"};
+
+    bd.add_child("li", "hello").add_child("li","world");
+
+    std::cout << bd.str() << std::endl;
+    
+    return 0;
+}
